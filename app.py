97edfeeb042b0e3742b35d2a770373e8ba4e9e5b
daithioc95 <daithioc@outlook.com>
2021-09-23 09:58:24 +0000

    Locate url key value from storeddata list

--- app.py
+++ app.py
@@ -11,40 +11,67 @@ import re
 filename = "datafiles/" + sys.argv[1]
 
 
-def ActionRequest(action):
-    r =requests.get(action['options']['url'])
-    storedData2 = r.text
-    parse_json = json.loads(storedData2)
-    # print(parse_json['ip'])
-    return parse_json
+def ActionRequest(action, data):
+    url = action['options']['url']
+    if "{{" and "}}" in url:
+        urlInterpolation(url, storedData)
+    else:
+        r =requests.get(url)
+        storedData2 = r.text
+        parse_json = json.loads(storedData2)
+        return parse_json
 
 
-# def urlInterpolation(url, inputdata):
-#      # s_without_parens = re.sub('\(.+?\)','',url)
-#     text_in_brackets = re.findall('[^{\{]+(?=}\})',url)
-#     keys=[]
-#     values=[]
-#     for i in text_in_brackets:
-#         keys.append(i.split(".")[0])
-#         values.append(i.split(".")[1])
-#     for value in inputdata:
-#         print("value")
+def urlInterpolation(url, inputdata):
+     # s_without_parens = re.sub('\(.+?\)','',url)
+    text_in_brackets = re.findall('[^{\{]+(?=}\})',url)
+    keys=[]
+    values = []
+    for i in text_in_brackets:
+        keys.append(i.split("."))
+    for j in range(0,len(keys)):
+        temp = storedData
+        for k in range(0,len(keys[j])):
+            temp=temp[keys[j][k]]
+        values.append(temp)
+    print(values)
+
+
+            # print(storedData['location']['latitude'])
+
+    # try mapping??
+    # print(*map(storedData.get, keys[1]))
+
+
+    # for i in keys:
+    #     if len(i)==2:
+    #         print(storedData[i[0]][i[1]])
+    #     elif len(i)==3:
+    #         print(storedData[i[0]][i[1]][i[0]])
+
+    # for i in range(0,len(keys)):
+    #     for j in keys[i]:
+    #         for x in storedData:
+    #             if j==x:
+    #                 for l in keys[i]:
+    #                     for y in storedData[x]:
+    #                         if l==y:
+    #                             print(storedData[x][l])
+    
+    # for item in keys:
+    #     for i in range(0,len(keys)-1):
+    #         print(item[i])
+    #         print(item[i+1])
+    #         print(storedData[item[i]][item[i+1]])
+    # print(keys)
+    # print(storedData[keys[0][0]][keys[0][1]])
 
 with open(filename) as json_file:
     actionsObject = json.load(json_file)
     storedData = {}
     for action in actionsObject['actions']:
         if action['type'] == "HTTPRequestAction":
-            # actionData = {action['name']:ActionRequest(action)}
-            # storedData.append(actionData)
-            storedData[action['name']] = ActionRequest(action)
-            parse_json2 = json.dumps(storedData)
-    print(storedData['location']['ip'])
-
-        # for x in storedData:
-        #     if action['name'] == list(x)[0]:
-        #         print(x)
-# print(storedData)
+            storedData[action['name']] = ActionRequest(action, storedData)
 
     # print(storedData[0]['location']['ip'])
             # r =requests.get(action['options']['url'])
